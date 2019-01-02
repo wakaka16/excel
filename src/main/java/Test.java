c0a31c1421ce6756d9899f170fbf0fec164067d0
王小龙 <wangxiaolong@vanda.com>
2019-01-02 18:41:31 +0800

    打印新增

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,36 +1,15 @@
-import excel.CreateExcel;
-import fileHandler.FileHandler;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import print.PrintUtils;
-import txt.GenerateGoodsInfo;
-import txt.GoodsInfo;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @Author wxl
- * @Date 2018/12/25
+ * @Date 2019/1/2
  **/
 public class Test {
 
-  public static void main(String[] args) throws IOException {
-//    System.out.println("hello exe");
-//    String dir = "C:/Users/58274/Desktop/连接螺栓定力记录表";
-//    List<File> allTxtFiles = FileHandler.getAllTxtFiles(dir);
-//    List<GoodsInfo> goodsInfoList = new ArrayList<GoodsInfo>();
-//    for (File txtFile:allTxtFiles) {
-//      List<String> stringList = GenerateGoodsInfo.readTxt(txtFile);
-//      GoodsInfo goodsInfo = GenerateGoodsInfo.createGoodsInfo(stringList);
-//      goodsInfoList.add(goodsInfo);
-//    }
-//    String path = "d:/test/test.xlsx";
-//    CreateExcel.createExcel(goodsInfoList,path );
-//    PrintUtils.printFileAction(path);
-    System.out.println("hello world");
-
-
-
+  public static void main(String[] args) {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+    System.out.println(sdf.format(new Date()));
   }
 
 }
